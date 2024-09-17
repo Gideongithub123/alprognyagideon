63bc825a79b226ad64edb4d21d22fa5f2fe46eeb
Gideon Suharman <gideoncrist98@gmail.com>
2024-09-18 01:40:31 +0800

    Soal3.c

--- Soal3.c
+++ Soal3.c
@@ -3,29 +3,32 @@
 #include <math.h>
 #include <stdlib.h>
 
-int isPrime(int n) {
+int isPrime(unsigned long long n) {
     if (n <= 1) {
         return 0; 
     }
     if (n == 2) {
-        return 1; 
+        return 1;
     }
     if (n % 2 == 0) {
-        return 0;
+        return 0; 
     }
-    
-  
-    for (int i = 3; i < n; i++) {
+
+    unsigned long long i;
+    unsigned long long limit = (unsigned long long)sqrt((double)n); 
+
+    for (i = 3; i <= limit; i += 2) { 
         if (n % i == 0) {
             return 0;
         }
     }
-    return 1; 
+    
+    return 1;
 }
 
 int main() {
-    int N;
-    scanf("%d", &N);
+    unsigned long long N;
+    scanf("%llu", &N);
     
     if (isPrime(N)) {
         printf("PRIMA\n");
